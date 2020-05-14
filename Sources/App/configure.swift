d86569a7e52ab625f43c64a8de407f3a6b120933
Maxim Krouk <id.maximkrouk@gmail.com>
2020-05-14 20:49:05 +0300

    first commit

---
+++ Sources/App/configure.swift
@@ -0,0 +1,21 @@
+import Fluent
+import FluentMySQLDriver
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    app.databases.use(.mysql(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    ), as: .mysql)
+
+    app.migrations.add(CreateTodo())
+
+    // register routes
+    try routes(app)
+}
