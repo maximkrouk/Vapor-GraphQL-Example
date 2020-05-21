f1a9ab39eeb2c81197cd2bdae538192e9d703d9a
maximkrouk <id.maximkrouk@gmail.com>
2020-05-21 20:10:48 +0300

    Cleanup

--- todos-backend/Sources/App/routes.swift
+++ todos-backend/Sources/App/routes.swift
@@ -12,51 +12,5 @@ func routes(_ app: Application) throws {
 
 private func setupGraphQL(_ app: Application) {
     let protected = app.grouped(JWTUserModelBearerAuthenticator())
-    
-    let schema = QLSchema<GraphQLAPIProvider, Request>([
-        QLType(UserModel.self, fields: [
-            QLField(.id, at: \.id),
-            QLField(.username, at: \.username)
-        ]),
-        
-        QLType(UserModel.Output.self, name: "UserModelOutput", fields: [
-            QLField(.id, at: \.id),
-            QLField(.username, at: \.username)
-        ]),
-        
-        QLType(UserModel.LoginResponse.self, name: "LoginResponse") {
-            QLField(.token, at: \.token)
-        },
-        
-        QLType(TodoModel.self, fields: [
-            QLField(.id, at: \.id),
-            QLField(.title, at: \.title),
-            QLField(.content, at: \.content)
-        ]),
-        
-        QLType(TodoModel.Output.self, name: "TodoModelOutput", fields: [
-            QLField(.id, at: \.id),
-            QLField(.title, at: \.title),
-            QLField(.content, at: \.content)
-        ]),
-        
-        QLScalar(HTTPResponseStatus.self, serialize: { status in
-            .int(Int(status.code))
-        }, parse: { map in
-            .init(statusCode: map.int ?? 0)
-        }),
-        
-        QLQuery([
-            QLField(GraphQLController.self, .todos) { $0.getTodos }
-        ]),
-        
-        QLMutation([
-            QLField(GraphQLController.self, .login) { $0.login },
-            QLField(GraphQLController.self, .register) { $0.register },
-            QLField(GraphQLController.self, .newTodo) { $0.postTodo },
-            QLField(GraphQLController.self, .deleteTodo) { $0.deleteTodo }
-        ])
-    ])
-    
-    protected.graphQL("graphQL", schema: schema, use: GraphQLAPIProvider())
+    protected.graphQL("graphQL", schema: app.makeGraphQLSchema(), use: GraphQLAPIProvider())
 }
